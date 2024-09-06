0600f498afa14b5d9d9fcbac09ecf083fd3b0ba9
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-09-06 15:24:41 +0200

    feat: automatically start md0 array

--- software/winapps.nix
+++ software/winapps.nix
@@ -15,6 +15,19 @@
   programs.virt-manager.enable = true;
   services.qemuGuest.enable = true;
 
+  systemd.services.virtual-disk-md0 = {
+    enable = true;
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+
+      ExecStart = "/home/oskar/.dotfiles/vm/start_md0";
+      ExecStop = "/home/oskar/.dotfiles/vm/stop_md0";
+    };
+
+    wantedBy = ["multi-user.target"];
+  };
+
   virtualisation.libvirtd = {
     enable = true;
   };
