722192203254d842f5a55b4d28267876ab7cdeae
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2025-05-29 12:23:13 +0200

    fmt

--- overlays/spotx.nix
+++ overlays/spotx.nix
@@ -1,23 +1,42 @@
 final: prev:
 let
   spotx = prev.fetchurl {
-    url =
-      "https://github.com/SpotX-Official/SpotX-Bash/raw/5e9b08f91e55c210bbc64715b4ad698186b3c06b/spotx.sh";
+    url = "https://github.com/SpotX-Official/SpotX-Bash/raw/5e9b08f91e55c210bbc64715b4ad698186b3c06b/spotx.sh";
     hash = "sha256-mlUc62b7Mw+Rpuiy0kqov5JCfrvKqgHLYfTf221gerM=";
   };
-in {
+in
+{
   spotify = prev.spotify.overrideAttrs (old: {
-    nativeBuildInputs = old.nativeBuildInputs
-      ++ (with prev; [ util-linux perl unzip zip curl ]);
+    nativeBuildInputs =
+      old.nativeBuildInputs
+      ++ (with prev; [
+        util-linux
+        perl
+        unzip
+        zip
+        curl
+      ]);
 
-    unpackPhase = builtins.replaceStrings [ "runHook postUnpack" ] [''
-      patchShebangs --build ${spotx}
-      runHook postUnpack
-    ''] old.unpackPhase;
+    unpackPhase =
+      builtins.replaceStrings
+        [ "runHook postUnpack" ]
+        [
+          ''
+            patchShebangs --build ${spotx}
+            runHook postUnpack
+          ''
+        ]
+        old.unpackPhase;
 
-    installPhase = builtins.replaceStrings [ "runHook postInstall" ] [''
-      bash ${spotx} -f -P "$out/share/spotify"
-      runHook postInstall
-    ''] old.installPhase;
+    installPhase =
+      builtins.replaceStrings
+        [ "runHook postInstall" ]
+        [
+          ''
+            bash ${spotx} -f -P "$out/share/spotify"
+            runHook postInstall
+          ''
+        ]
+        old.installPhase;
   });
 }
